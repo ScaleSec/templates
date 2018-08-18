3e827a3aede59d0795d61a3c967395e84340ded6
Aaron Wilson <aaron@scalesec.com>
2018-08-17 19:51:41 -0700

    add script to determine gcloud org user permissions

---
+++ list_user_permissions.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+# usage:
+# $ ./list_user_permissions.sh [org|project] [user_email]
+
+ORG_ID="340752610930"
+PROJ_ID="scalesec-dev"
+
+# paramaterize username
+#JQ_STRING=".bindings[] | select (.members[]==\"user:$1\") | .role"
+
+# find roles bound to this user
+ROLES=`gcloud organizations get-iam-policy ${ORG_ID} --format=json | jq --arg USER "user:$1" -r '.bindings[] | select (.members[]==$USER) | .role'`
+
+# list permission for each role and sort unique
+for ROLE in ${ROLES}; do
+	gcloud iam roles describe ${ROLE} --format=json | jq '.includedPermissions[]'; 
+done | sort -u
